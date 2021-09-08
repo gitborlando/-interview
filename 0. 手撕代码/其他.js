97165b8baf8bebc88773074e255c5ed8c6571e8f
Gitborlando <2629888564@qq.com>
2021-09-09 02:35:15 +0800

    feat(): 添加了闭包

--- 0. 手撕代码/其他.js
+++ 0. 手撕代码/其他.js
@@ -6,3 +6,24 @@ const New = (ctor, ...args) => {
   const res = ctor.apply(instance, args)
   return typeof res === 'object' ? res : instance
 }
+
+/**
+ * 深拷贝
+ */
+const deepClone = (obj, map = new Map()) => {
+  if (!obj instanceof Object) return obj
+  if (map.has(obj)) return map.get(obj)
+  const res = Array.isArray(obj) ? [] : {}
+  map.set(res, obj)
+  Object.entries(obj).forEach(([key, value]) => {
+    if (!obj.hasOwnProperty(key)) return true
+    if (typeof value === 'function') res[key] = value.bind(res)
+    if (value.constructor === Object || value.constructor === Array) {
+      res[key] = deepClone(value)
+    }
+    res[key] = value
+  })
+  return res
+}
+
+
